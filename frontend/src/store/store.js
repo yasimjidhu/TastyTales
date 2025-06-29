48df27d02a7dc034ef944683c1546a8159fec17f
yasimjidhu <dirshadyasim@gmail.com>
2025-06-29 23:08:32 +0530

    Implement Redux persist for recipe state management

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore } from 'redux-persist';
 import recipeReducer from '../store/slices/recipe';
 import userReducer from '../store/slices/user'
+import persistedRecipeReducer from './persist';
 
 const store = configureStore({
   reducer: {
-    recipes: recipeReducer,
+    recipes: persistedRecipeReducer,
     user: userReducer
   },
 });
 
+export const persistor = persistStore(store);
 export default store;
